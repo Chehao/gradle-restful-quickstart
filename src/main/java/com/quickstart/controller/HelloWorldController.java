daf725e7445ba3f5ecd452a968152a50efda1841
chehao.hsiao <chehao.hsiao@gmail.com>
2018-06-18 11:11:21 +0800

    update error handle

--- src/main/java/com/quickstart/controller/HelloWorldController.java
+++ src/main/java/com/quickstart/controller/HelloWorldController.java
@@ -1,23 +1,26 @@
 package com.quickstart.controller;
 
 import com.quickstart.api.Greeting;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 
-@Controller
+@RestController
 public class HelloWorldController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/hello-world")
-    @ResponseBody
     public Greeting sayHello(@RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/test")
+    public String testError() throws Exception {
+        throw new Exception("test exception");
     }
 }
